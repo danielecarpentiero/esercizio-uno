9b10ed65517b1c91b22d1bcb7301bbd8e925b981
Daniele Carpentiero <carpentierodaniele@gmail.com>
2023-11-24 21:59:01 +0100

    feat: conversione da fahrenheit a celsius

---
+++ esercizio.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int lower = 0;
+    int upper = 300;
+    int fahr;
+    int celsius;
+    int step = 20;
+
+    // Inizializza fahr a lower
+    fahr = lower;
+
+    while (fahr <= upper)
+    {
+        // Conversione da Fahrenheit a Celsius
+        celsius = 5 * (fahr - 32) / 9;
+        // Conversione da Fahrenheit a Celsius
+        printf("%d\t%d\n", fahr, celsius);
+        // Conversione da Fahrenheit a Celsius
+        fahr = fahr + step;
+    }
+    // Ritorno di un valore int dalla funzione main
+    return 0;
+}
